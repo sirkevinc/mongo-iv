46794e00c1a3908fdc9acc2a3f87e8d24c46a5dd
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-03-15 16:03:12 -0400

    Initial Commit - Progress from Lecture Walkthrough

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -5,5 +5,16 @@ const Character = require('./Character.js');
 const router = express.Router();
 
 // add endpoints here
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  Character.findById(id)
+    .populate('homeworld')
+    .then(character => {
+      res.status(200).json(character)
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Could not find Character'});
+    })
+});
 
 module.exports = router;
--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -5,5 +5,29 @@ const Film = require('./Film.js');
 const router = express.Router();
 
 // add endpoints here
+router.get('/', (req, res) => {
+  const producerFilter = req.query.producer;
+  const yearReleased = req.query.released;
+
+  let query = Film.find({})
+    .sort('episode')
+    .populate('characters', 'name gender height skin_color hair_color eye_color')
+    .populate('planets', 'name climate terrain gravity diameter')
+    .select('title producer')
+  
+    if (producerFilter) {
+    query.where({ producer: /gary kurtz/i });
+  }
+    if (yearReleased) {
+      query.where({ release_date: /2005/i})
+    }
+
+  query.then(films => {
+      res.status(200).json(films);
+    })
+    .catch(error => {
+      res.status(500).json({ errorMessage: 'Could not retrieve films.' });
+    })
+});
 
 module.exports = router;
